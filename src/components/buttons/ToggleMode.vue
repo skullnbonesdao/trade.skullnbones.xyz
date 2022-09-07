e0d90c78ed3eec80c6b135c8c02c78c2c21891e4
skullandbonesdao <skullandbonesdao@gmail.com>
2022-09-08 01:30:51 +0200

    added missing file to git

--- src/components/buttons/ToggleMode.vue
+++ src/components/buttons/ToggleMode.vue
@@ -1,31 +1,30 @@
 <template>
-  <button class="btn btn-secondary btn-circle" data-act-class="ACTIVECLASS" data-toggle-theme="business,corporate"
-          @click="theme_change"
+  <button
+    class="btn btn-secondary btn-circle"
+    data-act-class="ACTIVECLASS"
+    data-toggle-theme="business,corporate"
+    @click="theme_change"
   >
-    <i v-if="theme==='corporate'" class="bi bi-sun"></i>
-    <i v-if="theme==='business'" class=" bi bi-moon"></i>
+    <i v-if="theme === 'corporate'" class="bi bi-sun"></i>
+    <i v-if="theme === 'business'" class="bi bi-moon"></i>
   </button>
-
+  {{ theme }}
 </template>
 
 <script lang="ts" setup>
-import {onMounted, ref} from "vue";
+import { onMounted, ref } from "vue";
 
 const theme = ref();
 
-
 onMounted(() => {
-  theme.value = localStorage.getItem('theme');
-})
+  if (localStorage.getItem("theme")) {
+    theme.value = localStorage.getItem("theme");
+  } else {
+    theme.value = "corporate";
+  }
+});
 
 async function theme_change() {
-  theme.value = localStorage.getItem('theme');
+  theme.value = localStorage.getItem("theme");
 }
-
-
 </script>
-
-
-
-
-
