9c0a7fc1cb41785f67e059a18e8f622427d5946f
skullandbonesdao <skullandbonesdao@gmail.com>
2022-09-08 01:45:28 +0200

    added missing file to git 2

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,33 @@
-import {createApp} from 'vue'
+import { createApp } from "vue";
 //import './style.css'
-import App from './App.vue'
-import {createPinia} from 'pinia'
-import {createRouter, createWebHashHistory} from 'vue-router'
-import './index.css'
-import Home from '../src/views/HomeView.vue'
-import MarketplaceView from '../src/views/MarketplaceView.vue'
-import PersonToPersonView from '../src/views/PersonToPersonView.vue'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import { createRouter, createWebHashHistory } from "vue-router";
+import "./index.css";
+import Home from "../src/views/HomeView.vue";
+import MarketplaceView from "../src/views/MarketplaceView.vue";
+import PersonToPersonView from "../src/views/PersonToPersonView.vue";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "../src/assets/datafeeds/udf/dist/bundle";
 
-const pinia = createPinia()
+const pinia = createPinia();
 
-const About = {template: '<div>About</div>'}
-
+const About = { template: "<div>About</div>" };
 
 const routes = [
-    {path: '/', component: Home},
-    {path: '/marketplace', component: MarketplaceView},
-    {path: '/p2p', component: PersonToPersonView},
-    {path: '/about', component: About},
-]
+  { path: "/", component: Home },
+  { path: "/marketplace", component: MarketplaceView },
+  { path: "/p2p", component: PersonToPersonView },
+  { path: "/about", component: About },
+];
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: createWebHashHistory(),
-    routes, // short for `routes: routes`
-})
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+});
 
-
-createApp(App).use(pinia).use(router).mount('#app')
+createApp(App).use(pinia).use(router).mount("#app");
