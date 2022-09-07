f60efbc2debe44865b5732f5e9e51ceead53ae23
skullandbonesdao <skullandbonesdao@gmail.com>
2022-09-07 22:18:52 +0200

    added base

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    base: "/trade.skullnbones.xyz/",
+    plugins: [vue(),],
     optimizeDeps: {
         esbuildOptions: {
             target: 'es2020'
