f5e93407978fa96d31833bf71dab8c382b00ad9a
skullandbonesdao <skullandbonesdao@gmail.com>
2022-09-07 22:27:01 +0200

    added pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/trade.skullnbones.xyz/",
+    base: "./",
     plugins: [vue(),],
     optimizeDeps: {
         esbuildOptions: {
