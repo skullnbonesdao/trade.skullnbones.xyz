70ce17234f8722fbd1ff2bc066ddc3f435e5893a
skullandbonesdao <skullandbonesdao@gmail.com>
2022-09-08 00:03:42 +0200

    removed apexchart

--- src/views/MarketplaceView.vue
+++ src/views/MarketplaceView.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="">
-    <InfoFeed/>
+    <InfoFeed />
     <div class="flex">
       <div class="basis-1/2">
-        <SimplePriceHistory/>
-        <trading-view-chart/>
+        <trading-view-chart />
       </div>
       <div class="basis-1/2 flex">
-        <Orderbook/>
-        <OrderSetter/>
+        <Orderbook />
+        <OrderSetter />
       </div>
     </div>
-
   </div>
 </template>
 
 <script lang="ts" setup>
-import SimplePriceHistory from "../components/charts/apexcharts/SimplePriceHistory.vue";
 import InfoFeed from "../components/feeds/InfoFeed.vue";
 import Orderbook from "../components/trading/Orderbook.vue";
 import OrderSetter from "../components/trading/OrderSetter.vue";
-import TradingViewChart from "../components/charts/tradingview/TradingViewChart.vue";</script>
+import TradingViewChart from "../components/charts/tradingview/TradingViewChart.vue";
+</script>
