59790f2de63c9e5c285b301a56bd76dd76aeeee6
skullandbonesdao <skullandbonesdao@gmail.com>
2022-09-09 22:35:46 +0200

    added eslint

--- src/components/charts/tradingview/TradingViewChart.vue
+++ src/components/charts/tradingview/TradingViewChart.vue
@@ -1,38 +1,38 @@
 <template>
-  <div class="app">
-    <header class="app__header">
-      <h1 v-once class="app__title">
-        TradingView Charting Library and Vue.js Integration Example
-        {{ version }}
-      </h1>
-    </header>
-    <t-v-chart-container />
-  </div>
+    <div class="app">
+        <header class="app__header">
+            <h1 v-once class="app__title">
+                TradingView Charting Library and Vue.js Integration Example
+                {{ version }}
+            </h1>
+        </header>
+        <t-v-chart-container />
+    </div>
 </template>
 
 <script lang="ts" setup>
-import { version } from "../../../assets/charting_library";
-import TVChartContainer from "./TVChartContainer.vue";
+import { version } from '../../../assets/charting_library'
+import TVChartContainer from './TVChartContainer.vue'
 
 //const version:  version{}
 </script>
 
 <style lang="scss" scoped>
 .app {
-  text-align: center;
+    text-align: center;
 
-  &__header {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px 0;
-    background-color: #222;
-    color: #fff;
-  }
+    &__header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 10px 0;
+        background-color: #222;
+        color: #fff;
+    }
 
-  &__title {
-    display: block;
-    font-size: 1.5em;
-  }
+    &__title {
+        display: block;
+        font-size: 1.5em;
+    }
 }
 </style>
