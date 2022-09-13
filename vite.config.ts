60b0d887ee21ede1396eac61b51c606126c9f4d4
skullandbonesdao <skullandbonesdao@gmail.com>
2022-09-14 01:58:03 +0200

    build error fixed new buffer declaration

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import inject from "@rollup/plugin-inject";
+import * as path from "path";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
     base: './',
 
-    plugins: [
-        inject({
-            Buffer: ['buffer', 'Buffer'],
-        }),
-        vue(),
-    ],
+    plugins: [vue()],
     optimizeDeps: {
         esbuildOptions: {
             target: 'es2020',
         },
-        include: ['buffer'],
     },
     build: {
         target: 'es2020',
+    },
+    resolve: {
+        alias: {
+            process: 'process/browser',
+            stream: 'stream-browserify',
+            zlib: 'browserify-zlib',
+            util: 'util',
+            web3: path.resolve(__dirname, './node_modules/web3/dist/web3.min.js'),
+        },
     },
 })
