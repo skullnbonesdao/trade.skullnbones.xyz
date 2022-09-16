7cb5e6cb8b335de4b4a9152e742eb1a78252ff1c
DZWG <paule22@protonmail.com>
2022-09-16 23:40:02 +0200

    updated front

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
-  <h1>Weclome to SkullnBones Trading </h1>
-
-
+    <div class="bg-[url('/public/rouge_market_background_1.png')]">
+        <div class="pt-72 flex"></div>
+        <div class="flex flex-col justify-center text-center">
+            <h1>Rouge Market</h1>
+        </div>
+        <div class="flex flex-col justify-center text-center pt-9 pb-96">
+            <h2>HEIM and S&B</h2>
+        </div>
+    </div>
 </template>
 
+<script lang="ts" setup></script>
 
+<style lang="scss" scoped></style>
