9452727f9cdd09b174ed25a065fb9bbee94f05ba
skullandbonesdao <skullandbonesdao@gmail.com>
2022-09-22 23:13:02 +0200

    updated images and added orders view simple

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,38 @@
 <script lang="ts" setup>
 import NavBar from './components/navigation/NavBar.vue'
 import FooterComponent from './components/navigation/FooterComponent.vue'
+import { onMounted } from 'vue'
+import { useGlobalStore } from './stores/GlobalStore'
+import { useTokenPriceStore } from './stores/TokenPriceStore'
+import { useStaratlasGmStore } from './stores/StaratlasGmStore'
+import { useAssetsStore } from './stores/AssetsStore'
+import { useSolanaNetworkStore } from './stores/SolanaNetworkStore'
+
+onMounted(async () => {
+    const globalStore = useGlobalStore()
+    globalStore.init()
+
+    const tokenPriceWebsocket = useTokenPriceStore()
+    //tokenPriceWebsocket.init()
+    const staratlasGmStore = useStaratlasGmStore()
+
+    const solanaNetworkWebsocket = useSolanaNetworkStore()
+    solanaNetworkWebsocket.init()
+    solanaNetworkWebsocket.run_tps().catch((err) => console.error(`tps monitor: ${err}`))
+
+    const assetsStore = useAssetsStore()
+    await assetsStore.init()
+
+    useStaratlasGmStore()
+        .getOpenOrdersForAsset(
+            useAssetsStore().allAssets?.find((asset) => useGlobalStore().symbol.includes(asset.symbol))?.mint ?? ''
+        )
+        .then(() => {})
+        .catch((err) => console.log(err))
+    /*await useStaratlasGmStore().getOpenOrdersForAsset(
+useAssetsStore().allAssets?.find((asset) => useGlobalStore().symbol.includes(asset.symbol))?.mint ?? ''
+)*/
+})
 </script>
 
 <template>
