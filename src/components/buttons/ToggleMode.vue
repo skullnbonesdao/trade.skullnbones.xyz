65c481b46b5e3628af5dfa2981ca65eda6ee231a
skullandbonesdao <skullandbonesdao@gmail.com>
2022-10-11 19:40:24 +0200

    added tvChart Light/Dark Mode

--- src/components/buttons/ToggleMode.vue
+++ src/components/buttons/ToggleMode.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { useDark, useToggle } from '@vueuse/core'
+import { useGlobalStore } from '../../stores/GlobalStore'
 
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
+const globalStore = useGlobalStore()
 </script>
 
 <template>
-    <div class="flex hover:op80 justify-center m2 op30 text-2xl" @click="toggleDark(!isDark)">
+    <div class="flex hover:op80 justify-center m2 op30 text-2xl" @click="globalStore.toggleDdark()">
         <div class="color-black i-carbon-sun dark:i-carbon-asleep dark:color-white"></div>
     </div>
 </template>
--- src/components/charts/tradingview/TVChartContainer.vue
+++ src/components/charts/tradingview/TVChartContainer.vue
@@ -2,12 +2,12 @@
     <div ref="chartContainer" class="TVChartContainer" />
 </template>
 
+<!--
 <script>
-import { widget } from '../../../../public/charting_library'
-
 import { UDFCompatibleDatafeed } from '../../../typescript/tradingview_adapter/udf/lib/udf-compatible-datafeed.js'
 import { useGlobalStore } from '../../../stores/GlobalStore'
 import { storeToRefs } from 'pinia'
+import { useDark, useToggle } from '@vueuse/core'
 
 function getLanguageFromURL() {
     const regex = new RegExp('[\\?&]lang=([^&#]*)')
@@ -22,67 +22,13 @@ export default {
     setup() {
         const globalStore = useGlobalStore()
         const { symbol } = storeToRefs(globalStore)
+
         return {
             symbol,
         }
     },
 
-    mounted() {
-        const container = this.$refs.chartContainer
-        const widgetOptions = {
-            symbol: useGlobalStore().symbol, //assetSelected,
-            debug: false,
-            // BEWARE: no trailing slash is expected in feed URL
-            /*  default: 'http://localhost:3000',*/
-            /* default: '/api',*/
-            /* https://api2.skullnbones.xyz*/
-            datafeed: new UDFCompatibleDatafeed('https://api2.skullnbones.xyz/udf'),
-            interval: '60',
-            container: container,
-            library_path: '/charting_library/',
-            theme: 'dark',
-
-            locale: getLanguageFromURL() || 'en',
-            disabled_features: ['use_localstorage_for_settings'],
-            /*enabled_features: ["study_templates"],*/
-            /*      charts_storage_url: this.chartsStorageUrl,*/
-            charts_storage_api_version: '1.1',
-            client_id: 'clientID',
-            user_id: 'someID',
-            fullscreen: false,
-            autosize: true,
-            studies_overrides: {},
-        }
-
-        const tvWidget = new widget(widgetOptions)
-        this.tvWidget = tvWidget
-
-        tvWidget.onChartReady(() => {
-            tvWidget.headerReady().then(() => {
-                const button = tvWidget.createButton()
-
-                button.setAttribute('title', 'Click to show a notification popup')
-                button.classList.add('apply-common-tooltip')
-
-                button.addEventListener('click', () =>
-                    tvWidget.showNoticeDialog({
-                        title: 'Notification',
-                        body: 'TradingView Charting Library API works correctly',
-                        callback: () => {
-                            // eslint-disable-next-line no-console
-                            console.log('Noticed!')
-                        },
-                    })
-                )
-
-                button.innerHTML = 'Check API'
-            })
-            tvWidget
-                .activeChart()
-                .onSymbolChanged()
-                .subscribe(null, () => (useGlobalStore().symbol = tvWidget.activeChart().symbol()))
-        })
-    },
+    mounted() {},
 
     destroyed() {
         if (this.tvWidget !== null) {
@@ -90,6 +36,96 @@ export default {
             this.tvWidget = null
         }
     },
+}
+</script>
+-->
+
+<script setup>
+import { onMounted, watch } from 'vue'
+import { useDark, useToggle } from '@vueuse/core'
+import { UDFCompatibleDatafeed } from '../../../typescript/tradingview_adapter/udf/lib/udf-compatible-datafeed.js'
+/*import { widget } from '../../../../public/charting_library/charting_library.js'*/
+import { widget } from '../../../../public/charting_library'
+import { ref } from 'vue'
+import { useGlobalStore } from '../../../stores/GlobalStore'
+
+const tvWidget = ref(null)
+const chartContainer = ref(null)
+const globalStore = useGlobalStore()
+
+onMounted(() => {
+    createTVChart()
+})
+
+watch(
+    () => globalStore.is_dark,
+    async (is_dark) => {
+        createTVChart()
+    }
+)
+
+function getLanguageFromURL() {
+    const regex = new RegExp('[\\?&]lang=([^&#]*)')
+    const results = regex.exec(window.location.search)
+    return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '))
+}
+
+function createTVChart() {
+    const container = chartContainer.value
+    const chart_theme = globalStore.is_dark ? 'dark' : 'light'
+    const widgetOptions = {
+        symbol: useGlobalStore().symbol, //assetSelected,
+        debug: false,
+        // BEWARE: no trailing slash is expected in feed URL
+        /*  default: 'http://localhost:3000',*/
+        /* default: '/api',*/
+        /* https://api2.skullnbones.xyz*/
+        datafeed: new UDFCompatibleDatafeed('https://api2.skullnbones.xyz/udf'),
+        interval: '60',
+        container: container,
+        library_path: '/charting_library/',
+        theme: chart_theme,
+
+        locale: getLanguageFromURL() || 'en',
+        disabled_features: ['use_localstorage_for_settings'],
+        /*enabled_features: ["study_templates"],*/
+        /*      charts_storage_url: this.chartsStorageUrl,*/
+        charts_storage_api_version: '1.1',
+        client_id: 'clientID',
+        user_id: 'someID',
+        fullscreen: false,
+        autosize: true,
+        studies_overrides: {},
+    }
+
+    const tvWidget = new widget(widgetOptions)
+    tvWidget.value = tvWidget
+
+    tvWidget.onChartReady(() => {
+        tvWidget.headerReady().then(() => {
+            const button = tvWidget.createButton()
+
+            button.setAttribute('title', 'Click to show a notification popup')
+            button.classList.add('apply-common-tooltip')
+
+            button.addEventListener('click', () =>
+                tvWidget.showNoticeDialog({
+                    title: 'Notification',
+                    body: 'TradingView Charting Library API works correctly',
+                    callback: () => {
+                        // eslint-disable-next-line no-console
+                        console.log('Noticed!')
+                    },
+                })
+            )
+
+            button.innerHTML = 'Check API'
+        })
+        tvWidget
+            .activeChart()
+            .onSymbolChanged()
+            .subscribe(null, () => (useGlobalStore().symbol = tvWidget.activeChart().symbol()))
+    })
 }
 </script>
 
