698b1808e7e9b49654a849e851060b84a1e514de
skullandbonesdao <skullandbonesdao@gmail.com>
2022-10-14 00:28:50 +0200

    added icons/symbol for assets

---
+++ src/components/badges/AssetImageNameBadge.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex flex-col static place-items-center">
+        <div class="avatar">
+            <div class="w-12 rounded">
+                <img class="rounded-md" :src="img_url" alt="asset_image" />
+            </div>
+        </div>
+        <div class="absolute pt-9">
+            <div class="badge">{{ symbol }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    symbol: { type: String, default: 'SOME' },
+    img_url: {
+        type: String,
+        default:
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.NoU3q4AEZO6bFNhD4RerAQHaE9%26pid%3DApi&f=1',
+    },
+})
+</script>
+
+<style scoped></style>
