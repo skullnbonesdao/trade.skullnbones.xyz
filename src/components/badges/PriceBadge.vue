7935a70c6cf2fe91568afb8ac825b15ebf48bec6
skullnbonesdao <skullandbonesdao@gmail.com>
2022-10-24 23:16:34 +0200

    updated tickers

--- src/components/badges/PriceBadge.vue
+++ src/components/badges/PriceBadge.vue
@@ -3,24 +3,22 @@
         <div class="w-9 pt-1">
             <img class="" :src="'/tokens/' + props.imageName + '.webp'" :alt="props.imageName" />
         </div>
-        <div class="flex flex-row w-28">
-            <div class="">
+
+        <div>
+            <div class="flex items-center">
                 <div class="text-right">{{ props.price }}</div>
-                <div v-if="props.change24h > 0" class="text-right text-green-400">
-                    {{ props.change24h.toFixed(2) }}
-                </div>
-                <div v-if="props.change24h <= 0" class="text-right text-red-400">
-                    {{ props.change24h.toFixed(2) }}
-                </div>
-            </div>
-            <div>
                 <div class="text-sm i-carbon-currency-dollar"></div>
-                <div v-if="props.change24h > 0" class="text-sm i-carbon-percentage text-green-400"></div>
-                <div v-if="props.change24h <= 0" class="text-sm i-carbon-percentage text-red-400"></div>
-                <div class="space-x-0.5">
-                    <div v-if="props.change24h > 0" class="text-sm i-carbon-arrow-up-right text-green-400"></div>
-                    <div v-if="props.change24h <= 0" class="text-sm i-carbon-arrow-down-right text-red-400"></div>
+            </div>
+
+            <div class="flex items-center">
+                <div
+                    class="text-sm"
+                    :class="change24h > 0 ? 'i-carbon-arrow-up-right text-green' : 'i-carbon-arrow-down-right text-red'"
+                ></div>
+                <div class="text-right" :class="change24h > 0 ? 'text-green' : 'text-red'">
+                    {{ props.change24h.toFixed(2) }}
                 </div>
+                <div class="text-sm i-carbon-percentage" :class="change24h > 0 ? 'text-green' : 'text-red'"></div>
             </div>
         </div>
     </div>
