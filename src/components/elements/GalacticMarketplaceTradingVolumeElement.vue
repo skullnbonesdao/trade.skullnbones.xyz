2127d39cdba42c81b9e1f8751550c77dd716cdf1
DZWG <paule22@protonmail.com>
2023-03-18 19:31:13 +0100

    added merge function
    Signed-off-by: DZWG <paule22@protonmail.com>

--- src/components/elements/GalacticMarketplaceTradingVolumeElement.vue
+++ src/components/elements/GalacticMarketplaceTradingVolumeElement.vue
@@ -26,14 +26,31 @@ export default {
                         '08/01/2003',
                     ],
                 },
+                yaxis: [
+                    {
+                        opposite: true,
+                        seriesName: 'ATLAS',
+                        min: 0,
+                        // labels: {
+                        //     formatter: this.label_formatter,
+                        // },
+                    },
+                    {
+                        seriesName: 'USDC',
+                        min: 0,
+                        // labels: {
+                        //     formatter: this.label_formatter,
+                        // },
+                    },
+                ],
             },
             series: [
                 {
-                    name: 'Series A',
+                    name: 'ATLAS',
                     data: [],
                 },
                 {
-                    name: 'Series B',
+                    name: 'USDC',
                     data: [],
                 },
             ],
@@ -45,28 +62,88 @@ export default {
             this.show_chart = false
 
             const api = new Api({ baseUrl: 'https://api2.skullnbones.xyz' })
-            let series = [
-                { name: 'ATLAS', data: [] },
-                { name: 'USDC', data: [] },
-            ]
-            await api.trades
-                .getVolume({ currency_mint: CURRENCIES.find((c) => c.type === E_CURRENCIES.ATLAS).mint })
-                .then((resp) => resp.data)
-                .then((data) => {
-                    series[0].data = data.flatMap((d) => d.volume)
-                })
+
+            let array_1 = []
+            let array_2 = []
 
             await api.trades
                 .getVolume({ currency_mint: CURRENCIES.find((c) => c.type === E_CURRENCIES.ATLAS).mint })
                 .then((resp) => resp.data)
                 .then((data) => {
-                    series[1].data = data.flatMap((d) => d.volume)
-                    this.chartOptions.xaxis.categories = []
-                    this.chartOptions.xaxis.categories = data.flatMap((d) => d.time)
+                    console.log('ATLAS')
                     console.log(data)
+                    //this.series[0].data = data.flatMap((d) => d.volume)
+                    array_1 = data
                 })
-            this.series = series
+
+            await api.trades
+                .getVolume({ currency_mint: CURRENCIES.find((c) => c.type === E_CURRENCIES.USDC).mint })
+                .then((resp) => resp.data)
+                .then((data) => {
+                    array_2 = data
+                })
+            let array_combined = this.mergeArrays(array_1, array_2)
+
+            this.series[0].data = array_combined.map((el) => el.volume_1)
+            this.series[1].data = array_combined.map((el) => el.volume_2)
+            this.chartOptions.xaxis.categories = array_combined.map((el) => el.time)
+            console.log(array_combined)
             this.show_chart = true
+        },
+        label_formatter(value) {
+            return value.toFixed(2)
+        },
+        mergeArrays(arr1, arr2) {
+            let mergedArr = []
+
+            // iterate over the first array
+            for (let i = 0; i < arr1.length; i++) {
+                let time = arr1[i].time
+                let volume1 = arr1[i].volume
+                let volume2 = 0
+
+                // iterate over the second array to find a match
+                for (let j = 0; j < arr2.length; j++) {
+                    if (arr2[j].time === time) {
+                        volume2 = arr2[j].volume
+                        break
+                    }
+                }
+
+                // add the volumes together and push the result to the merged array
+                mergedArr.push({
+                    time: time,
+                    volume_1: volume1,
+                    volume_2: volume2,
+                })
+            }
+
+            // iterate over the second array to find unmatched times
+            for (let i = 0; i < arr2.length; i++) {
+                let time = arr2[i].time
+                let volume1 = 0
+                let volume2 = arr2[i].volume
+
+                // iterate over the merged array to check for a match
+                let matched = false
+                for (let j = 0; j < mergedArr.length; j++) {
+                    if (mergedArr[j].time === time) {
+                        matched = true
+                        break
+                    }
+                }
+
+                // if there is no match, push the missing data to the merged array
+                if (!matched) {
+                    mergedArr.push({
+                        time: time,
+                        volume_1: volume1,
+                        volume_2: volume2,
+                    })
+                }
+            }
+
+            return mergedArr
         },
     },
     mounted() {
