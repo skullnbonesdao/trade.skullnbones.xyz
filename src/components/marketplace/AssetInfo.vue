440b618769714cf97a27bca9d06cfe598f9e0cf7
DZWG <paule22@protonmail.com>
2023-03-19 22:57:44 +0100

    fixed price change
    Signed-off-by: DZWG <paule22@protonmail.com>

--- src/components/marketplace/AssetInfo.vue
+++ src/components/marketplace/AssetInfo.vue
@@ -61,16 +61,19 @@
                 </div>
             </div>
             <div class="flex w-full flex-row items-center space-x-2 justify-end">
+                <CurrencyIcon
+                    class="w-6 h-6"
+                    :currency="CURRENCIES.find((c) => useGlobalStore().symbol.mint_pair.toString() === c.mint)"
+                />
                 <div>
                     <div class="text-right">
-                        {{ price_24_h?.toFixed(2)
-                        }}{{ CURRENCIES.find((c) => useGlobalStore().symbol.mint_pair.toString() === c.mint)?.char }}
+                        <p>{{ price_last?.toFixed(4) }}</p>
                     </div>
                     <div class="flex items-center float-right">
                         <div
                             class="text-sm"
                             :class="
-                                price_24_change === 0
+                                price_24_change === 1
                                     ? 'i-carbon-arrow-right text-gray'
                                     : price_24_change >= 1
                                     ? 'i-carbon-arrow-up-right text-green'
@@ -81,10 +84,14 @@
                         <div
                             class="text-right"
                             :class="
-                                price_24_change === 0 ? 'text-gray' : price_24_change > 1 ? 'text-green' : 'text-red'
+                                price_24_change === 1 ? 'text-gray' : price_24_change > 1 ? 'text-green' : 'text-red'
                             "
                         >
-                            {{ price_24_change >= 1 ? (price_24_change - 1).toFixed(2) : price_24_change.toFixed(2) }}%
+                            {{
+                                price_24_change >= 1
+                                    ? (price_24_change - 1).toFixed(2)
+                                    : (1 - price_24_change).toFixed(2)
+                            }}%
                         </div>
                     </div>
                 </div>
@@ -113,11 +120,14 @@ import AssetRarityBadge from '../badges/AssetRarityBadge.vue'
 import AssetItemTypeBadge from '../badges/AssetItemTypeBadge.vue'
 import AssetTextBadge from '../badges/AssetTextBadge.vue'
 import { Api, Trade } from '../../typescript/skullnbones_api/skullnbones_api'
+import CurrencyIcon from '../icon-helper/CurrencyIcon.vue'
 const api = new Api({ baseUrl: 'https://api2.skullnbones.xyz' })
 
 const show_search_modal = ref(false)
 const price_24_h = ref(0.0)
+const price_last = ref(0.0)
 const price_24_change = ref(0.0)
+
 fetch_price_24_h()
 
 watch(useGlobalStore().symbol, (new_value) => {
@@ -137,7 +147,7 @@ async function action_update_symbol(symbol: string) {
     console.log(symbol)
 }
 
-function fetch_price_24_h() {
+async function fetch_price_24_h() {
     let tsNow = Math.round(new Date().getTime() / 1000)
     let tsYesterday = tsNow - 24 * 3600
     let api_trade: Trade[] = []
@@ -145,23 +155,22 @@ function fetch_price_24_h() {
     let price_temp = 0
 
     //Get trade now-24h
-    api.trades
+    await api.trades
         .getSymbol({ symbol: useGlobalStore().symbol?.name, limit: 1, to: tsYesterday })
         .then((resp) => resp.data)
         .then((data) => {
             console.log(data[0])
-            price_temp = data[0].price
+            price_24_h.value = data[0].price
         })
     //Get trade now
-    api.trades
+    await api.trades
         .getSymbol({ symbol: useGlobalStore().symbol?.name, limit: 1, to: tsNow })
         .then((resp) => resp.data)
         .then((data) => {
             console.log(data[0])
-
-            price_24_change.value = price_temp / data[0].price
-            price_24_h.value = data[0].price
+            price_last.value = data[0].price
         })
+    price_24_change.value = price_24_h.value / price_last.value
 }
 </script>
 
