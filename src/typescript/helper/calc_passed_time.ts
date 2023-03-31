0ac5579ae85abaf2a25ef6d261d25aa5da9e299c
DZWG <paule22@protonmail.com>
2023-03-31 18:50:03 +0200

    updated table format
    Signed-off-by: DZWG <paule22@protonmail.com>

--- src/typescript/helper/calc_passed_time.ts
+++ src/typescript/helper/calc_passed_time.ts
@@ -5,5 +5,5 @@ export function calc_passed_time(timestamp_to_get_since: number): String {
     let diffDays = Math.floor(diffMs / 86400000) // days
     let diffHrs = Math.floor((diffMs % 86400000) / 3600000) // hours
     let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000) // minutes
-    return diffDays + ' d, ' + diffHrs + ' h, ' + diffMins + ' min'
+    return diffDays + 'd, ' + diffHrs + 'h, ' + diffMins + 'min'
 }
